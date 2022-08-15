09c99e8378a0c06e13315e0b96f28816dd8d666d
Devika <89661349+Devikaiii@users.noreply.github.com>
2022-08-15 20:01:37 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 echo "Cloning Repo, Please Wait..."
 git clone -b master https://github.com/Devikaiii/MJ.git /Devikaiii
-cd /devikaiii
+cd /Devikaiii
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
 python3 bot.py
+
